openshift/openshift-extras

commit 8b2d53cd722590d6142872ca4b175b24ff7f8588

add support for activation key registration

enterprise/install-scripts/amazon/openshift-amz.sh
@@ -198,7 +198,7 @@ install_cartridges()
   # JBossEAP6.0 support.
   # Note: Be sure to subscribe to the JBossEAP entitlements during the
   # base install or in configure_jbosseap_subscription.
-  #carts="$carts openshift-origin-cartridge-jbosseap-6.0"
+  carts="$carts openshift-origin-cartridge-jbosseap-6.0"
 
   # Jenkins server for continuous integration.
   carts="$carts openshift-origin-cartridge-jenkins-1.4"
@@ -229,7 +229,10 @@ install_cartridges()
 
   # When dependencies are missing, e.g. JBoss subscriptions,
   # still install as much as possible.
-  carts="$carts --skip-broken"
+  if [ "$CONF_OPTIONAL_REPO" == 1 ]
+  then
+    carts="$carts --skip-broken"
+  fi
 
   yum install -y $carts
 }
@@ -429,7 +432,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
+  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF
@@ -1538,7 +1541,7 @@ echo_installation_intentions
 is_false "$CONF_NO_NTP" && synchronize_clock
 is_false "$CONF_NO_SSH_KEYS" && install_ssh_keys
 
-if is_false "$CONF_NO_REPOS"
+if [ "$CONF_INSTALL_METHOD" == "yum" ]
 then
   configure_rhel_repo
   if activemq || broker || datastore
@@ -1547,9 +1550,27 @@ then
   fi
   node && configure_node_repo
   node && configure_jbosseap_cartridge_repo
-  node && configure_jbosseap_subscription
-  node && configure_jbossews_subscription
   broker && configure_client_tools_repo
+elif [ "$CONF_INSTALL_METHOD" == "rhn" ]
+then
+   echo "Register with RHN using an activation key"
+   rhnreg_ks --activationkey=${CONF_RHN_REG_ACTKEY} --profilename=${hostname}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-rhc --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-infrastructure --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-node --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-jbosseap --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jbappplatform-6-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jb-ews-1-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   if [ "$CONF_OPTIONAL_REPO" == 1 ]
+   then
+     rhn-channel --add --channel rhel-x86_64-server-optional-6 --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   fi
+elif [ "$CONF_INSTALL_METHOD" == "sm" ]
+then
+# sm_reg_name / CONF_SM_REG_NAME
+# sm_reg_pass / CONF_SM_REG_PASS
+# sm_reg_pool / CONF_SM_REG_POLL
+   echo "sam"
 fi
 
 yum update -y

enterprise/install-scripts/generic/openshift.ks
@@ -37,35 +37,40 @@
 #  install_components="node,broker,named,activemq,datastore"
 # Example script variable:
 #  CONF_INSTALL_COMPONENTS="node,broker,named,activemq,datastore"
+#CONF_INSTALL_COMPONENTS="node"
 
 # install_method / CONF_INSTALL_METHOD
 #   Choice from the following ways to provide packages:
 #     yum - set up yum repos manually
 #     sm - use subscription-manager
+#       sm_reg_name / CONF_SM_REG_NAME
+#       sm_reg_pass / CONF_SM_REG_PASS
+#       sm_reg_pool / CONF_SM_REG_POLL
 #     rhn - use rhn-register
+#       rhn_reg_name / CONF_RHN_REG_NAME
+#       rhn_reg_pass / CONF_RHN_REG_PASS
+#       rhn_reg_actkey / CONF_RHN_REG_ACTKEY
 #     none - install source is already set up when the script executes
 #   Default: yum
+#CONF_INSTALL_METHOD="yum"
 
-# sm_reg_name / CONF_SM_REG_NAME
-# sm_reg_pass / CONF_SM_REG_PASS
-# sm_reg_pool / CONF_SM_REG_POLL
-#   If using the "sm" install method, credentials for registering and
-#   subscribing.
-
-# rhn_reg_name / CONF_RHN_REG_NAME
-# rhn_reg_pass / CONF_RHN_REG_PASS
-# rhn_reg_subid / CONF_RHN_REG_SUBID
-#   If using the "sm" install method, credentials for registering and
-#   subscribing.
+# optional_repo / CONF_OPTIONAL_REPO
+#   Setup rhel-x86_64-server-optional-6 repo and add --skip-broken to
+#   catch the use case when dependencies are missing, e.g. JBoss
+#   subscriptions, still install as much as possible.
+#   Default: no
+#CONF_OPTIONAL_REPO=1
 
 # repos_base / CONF_REPOS_BASE
 #   Default: https://mirror.openshift.com/pub/origin-server/nightly/enterprise/<latest>
 #   The base URL for the OpenShift repositories used for the "yum" 
 #   install method
+#CONF_REPOS_BASE="https://mirror.openshift.com/pub/origin-server/nightly/enterprise/<latest>"
 
 # domain / CONF_DOMAIN
 #   Default: example.com
 #   The network domain under which apps and hosts will be placed.
+#CONF_DOMAIN="example.com"
 
 # broker_hostname / CONF_BROKER_HOSTNAME
 # node_hostname / CONF_NODE_HOSTNAME
@@ -82,26 +87,35 @@
 #   DNS entries for the hostnames of the other components being 
 #   installed on this host as well. If you are using a nameserver set
 #   up separately, you are responsible for all necessary DNS entries.
+#CONF_BROKER_HOSTNAME="broker.example.com"
+#CONF_NODE_HOSTNAME="node.example.com"
+#CONF_NAMED_HOSTNAME="ns1.example.com"
+#CONF_ACTIVEMQ_HOSTNAME="activemq.example.com"
+#CONF_DATASTORE_HOSTNAME="mongodb.example.com"
 
 
 # named_ip_addr / CONF_NAMED_IP_ADDR
 #   Default: current IP if installing named, otherwise broker_ip_addr
 #   This is used by every host to configure its primary nameserver.
+#CONF_NAMED_IP_ADDR=10.10.10.10
 
 # bind_key / CONF_BIND_KEY
 #   When the nameserver is remote, use this to specify the HMAC-MD5 key
 #   for updates. This is the "Key:" field from the .private key file
 #   generated by dnssec-keygen.
+#CONF_BIND_KEY=""
 
 # broker_ip_addr / CONF_BROKER_IP_ADDR
 #   Default: the current IP (at install)
 #   This is used for the node to record its broker. Also is the default
 #   for the nameserver IP if none is given.
+#CONF_BROKER_IP_ADDR=10.10.10.10
 
 # node_ip_addr / CONF_NODE_IP_ADDR
 #   Default: the current IP (at install)
 #   This is used for the node to give a public IP, if different from the
 #   one on its NIC.
+#CONF_NODE_IP_ADDR=10.10.10.10
 
 # Passwords used to secure various services. You are advised to specify
 # only alphanumeric values in this script as others may cause syntax
@@ -112,14 +126,18 @@
 #   Default: randomized
 #   This is the admin password for the ActiveMQ admin console, which is
 #   not needed by OpenShift but might be useful in troubleshooting.
-#
+#CONF_ACTIVEMQ_ADMIN_PASSWORD="ChangeMe"
+
+
 # mcollective_user / CONF_MCOLLECTIVE_USER
 # mcollective_password / CONF_MCOLLECTIVE_PASSWORD
 #   Default: mcollective/marionette
 #   This is the user and password shared between broker and node for
 #   communicating over the mcollective topic channels in ActiveMQ. Must
 #   be the same on all broker and node hosts.
-#
+#CONF_MCOLLECTIVE_USER="mcollective"
+#CONF_MCOLLECTIVE_PASSWORD="mcollective"
+
 # mongodb_admin_user / CONF_MONGODB_ADMIN_USER
 # mongodb_admin_password / CONF_MONGODB_ADMIN_PASSWORD
 #   Default: admin:mongopass
@@ -129,7 +147,9 @@
 #   must be added to MongoDB in order for it to enforce authentication.
 #   Note: The administrative user will not be created if
 #   CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST is enabled.
-#
+#CONF_MONGODB_ADMIN_USER="admin"
+#CONF_MONGODB_ADMIN_PASSWORD="mongopass"
+
 # mongodb_broker_user / CONF_MONGODB_BROKER_USER
 # mongodb_broker_password / CONF_MONGODB_BROKER_PASSWORD
 #   Default: openshift:mongopass
@@ -137,18 +157,23 @@
 #   created for the broker to connect to the MongoDB datastore. The
 #   broker application's MongoDB plugin is also configured with these
 #   values.
-#
+#CONF_MONGODB_BROKER_USER="openshift"
+#CONF_MONGODB_BROKER_PASSWORD="mongopass"
+
 # mongodb_name / CONF_MONGODB_NAME
 #   Default: openshift_broker
 #   This is the name of the database in MongoDB in which the broker will
 #   store data.
-#
+#CONF_MONGODB_NAME="openshift_broker"
+
 # openshift_user1 / CONF_OPENSHIFT_USER1
 # openshift_password1 / CONF_OPENSHIFT_PASSWORD1
 #   Default: demo/changeme
 #   This user and password are entered in the /etc/openshift/htpasswd
 #   file as a demo/test user. You will likely want to remove it after
 #   installation (or just use a different auth method).
+#CONF_OPENSHIFT_USER1="demo"
+#CONF_OPENSHIFT_PASSWORD1="changeme"
 
 # IMPORTANT NOTES - DEPENDENCIES
 #
@@ -476,7 +501,7 @@ install_cartridges()
   # JBossEAP6.0 support.
   # Note: Be sure to subscribe to the JBossEAP entitlements during the
   # base install or in configure_jbosseap_subscription.
-  #carts="$carts openshift-origin-cartridge-jbosseap-6.0"
+  carts="$carts openshift-origin-cartridge-jbosseap-6.0"
 
   # Jenkins server for continuous integration.
   carts="$carts openshift-origin-cartridge-jenkins-1.4"
@@ -507,7 +532,10 @@ install_cartridges()
 
   # When dependencies are missing, e.g. JBoss subscriptions,
   # still install as much as possible.
-  carts="$carts --skip-broken"
+  if [ "$CONF_OPTIONAL_REPO" == 1 ]
+  then
+    carts="$carts --skip-broken"
+  fi
 
   yum install -y $carts
 }
@@ -707,7 +735,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
+  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF
@@ -1817,7 +1845,7 @@ configure_console_msg
 is_false "$CONF_NO_NTP" && synchronize_clock
 is_false "$CONF_NO_SSH_KEYS" && install_ssh_keys
 
-if is_false "$CONF_NO_REPOS"
+if [ "$CONF_INSTALL_METHOD" == "yum" ]
 then
   configure_rhel_repo
   if activemq || broker || datastore
@@ -1826,9 +1854,27 @@ then
   fi
   node && configure_node_repo
   node && configure_jbosseap_cartridge_repo
-  node && configure_jbosseap_subscription
-  node && configure_jbossews_subscription
   broker && configure_client_tools_repo
+elif [ "$CONF_INSTALL_METHOD" == "rhn" ]
+then
+   echo "Register with RHN using an activation key"
+   rhnreg_ks --activationkey=${CONF_RHN_REG_ACTKEY} --profilename=${hostname}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-rhc --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-infrastructure --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-node --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-jbosseap --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jbappplatform-6-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jb-ews-1-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   if [ "$CONF_OPTIONAL_REPO" == 1 ]
+   then
+     rhn-channel --add --channel rhel-x86_64-server-optional-6 --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   fi
+elif [ "$CONF_INSTALL_METHOD" == "sm" ]
+then
+# sm_reg_name / CONF_SM_REG_NAME
+# sm_reg_pass / CONF_SM_REG_PASS
+# sm_reg_pool / CONF_SM_REG_POLL
+   echo "sam"
 fi
 
 yum update -y

enterprise/install-scripts/generic/openshift.sh
@@ -38,37 +38,40 @@
 #  install_components="node,broker,named,activemq,datastore"
 # Example script variable:
 #  CONF_INSTALL_COMPONENTS="node,broker,named,activemq,datastore"
-CONF_INSTALL_COMPONENTS="broker,named,activemq,datastore"
+#CONF_INSTALL_COMPONENTS="node"
 
 # install_method / CONF_INSTALL_METHOD
 #   Choice from the following ways to provide packages:
 #     yum - set up yum repos manually
 #     sm - use subscription-manager
+#       sm_reg_name / CONF_SM_REG_NAME
+#       sm_reg_pass / CONF_SM_REG_PASS
+#       sm_reg_pool / CONF_SM_REG_POLL
 #     rhn - use rhn-register
+#       rhn_reg_name / CONF_RHN_REG_NAME
+#       rhn_reg_pass / CONF_RHN_REG_PASS
+#       rhn_reg_actkey / CONF_RHN_REG_ACTKEY
 #     none - install source is already set up when the script executes
 #   Default: yum
+#CONF_INSTALL_METHOD="yum"
 
-# sm_reg_name / CONF_SM_REG_NAME
-# sm_reg_pass / CONF_SM_REG_PASS
-# sm_reg_pool / CONF_SM_REG_POLL
-#   If using the "sm" install method, credentials for registering and
-#   subscribing.
-
-# rhn_reg_name / CONF_RHN_REG_NAME
-# rhn_reg_pass / CONF_RHN_REG_PASS
-# rhn_reg_subid / CONF_RHN_REG_SUBID
-#   If using the "sm" install method, credentials for registering and
-#   subscribing.
+# optional_repo / CONF_OPTIONAL_REPO
+#   Setup rhel-x86_64-server-optional-6 repo and add --skip-broken to
+#   catch the use case when dependencies are missing, e.g. JBoss
+#   subscriptions, still install as much as possible.
+#   Default: no
+#CONF_OPTIONAL_REPO=1
 
 # repos_base / CONF_REPOS_BASE
 #   Default: https://mirror.openshift.com/pub/origin-server/nightly/enterprise/<latest>
 #   The base URL for the OpenShift repositories used for the "yum" 
 #   install method
+#CONF_REPOS_BASE="https://mirror.openshift.com/pub/origin-server/nightly/enterprise/<latest>"
 
 # domain / CONF_DOMAIN
 #   Default: example.com
 #   The network domain under which apps and hosts will be placed.
-CONF_DOMAIN="pryzby.org"
+#CONF_DOMAIN="example.com"
 
 # broker_hostname / CONF_BROKER_HOSTNAME
 # node_hostname / CONF_NODE_HOSTNAME
@@ -85,26 +88,35 @@ CONF_DOMAIN="pryzby.org"
 #   DNS entries for the hostnames of the other components being 
 #   installed on this host as well. If you are using a nameserver set
 #   up separately, you are responsible for all necessary DNS entries.
+#CONF_BROKER_HOSTNAME="broker.example.com"
+#CONF_NODE_HOSTNAME="node.example.com"
+#CONF_NAMED_HOSTNAME="ns1.example.com"
+#CONF_ACTIVEMQ_HOSTNAME="activemq.example.com"
+#CONF_DATASTORE_HOSTNAME="mongodb.example.com"
 
 
 # named_ip_addr / CONF_NAMED_IP_ADDR
 #   Default: current IP if installing named, otherwise broker_ip_addr
 #   This is used by every host to configure its primary nameserver.
+#CONF_NAMED_IP_ADDR=10.10.10.10
 
 # bind_key / CONF_BIND_KEY
 #   When the nameserver is remote, use this to specify the HMAC-MD5 key
 #   for updates. This is the "Key:" field from the .private key file
 #   generated by dnssec-keygen.
+#CONF_BIND_KEY=""
 
 # broker_ip_addr / CONF_BROKER_IP_ADDR
 #   Default: the current IP (at install)
 #   This is used for the node to record its broker. Also is the default
 #   for the nameserver IP if none is given.
+#CONF_BROKER_IP_ADDR=10.10.10.10
 
 # node_ip_addr / CONF_NODE_IP_ADDR
 #   Default: the current IP (at install)
 #   This is used for the node to give a public IP, if different from the
 #   one on its NIC.
+#CONF_NODE_IP_ADDR=10.10.10.10
 
 # Passwords used to secure various services. You are advised to specify
 # only alphanumeric values in this script as others may cause syntax
@@ -115,14 +127,18 @@ CONF_DOMAIN="pryzby.org"
 #   Default: randomized
 #   This is the admin password for the ActiveMQ admin console, which is
 #   not needed by OpenShift but might be useful in troubleshooting.
-#
+#CONF_ACTIVEMQ_ADMIN_PASSWORD="ChangeMe"
+
+
 # mcollective_user / CONF_MCOLLECTIVE_USER
 # mcollective_password / CONF_MCOLLECTIVE_PASSWORD
 #   Default: mcollective/marionette
 #   This is the user and password shared between broker and node for
 #   communicating over the mcollective topic channels in ActiveMQ. Must
 #   be the same on all broker and node hosts.
-#
+#CONF_MCOLLECTIVE_USER="mcollective"
+#CONF_MCOLLECTIVE_PASSWORD="mcollective"
+
 # mongodb_admin_user / CONF_MONGODB_ADMIN_USER
 # mongodb_admin_password / CONF_MONGODB_ADMIN_PASSWORD
 #   Default: admin:mongopass
@@ -132,7 +148,9 @@ CONF_DOMAIN="pryzby.org"
 #   must be added to MongoDB in order for it to enforce authentication.
 #   Note: The administrative user will not be created if
 #   CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST is enabled.
-#
+#CONF_MONGODB_ADMIN_USER="admin"
+#CONF_MONGODB_ADMIN_PASSWORD="mongopass"
+
 # mongodb_broker_user / CONF_MONGODB_BROKER_USER
 # mongodb_broker_password / CONF_MONGODB_BROKER_PASSWORD
 #   Default: openshift:mongopass
@@ -140,18 +158,23 @@ CONF_DOMAIN="pryzby.org"
 #   created for the broker to connect to the MongoDB datastore. The
 #   broker application's MongoDB plugin is also configured with these
 #   values.
-#
+#CONF_MONGODB_BROKER_USER="openshift"
+#CONF_MONGODB_BROKER_PASSWORD="mongopass"
+
 # mongodb_name / CONF_MONGODB_NAME
 #   Default: openshift_broker
 #   This is the name of the database in MongoDB in which the broker will
 #   store data.
-#
+#CONF_MONGODB_NAME="openshift_broker"
+
 # openshift_user1 / CONF_OPENSHIFT_USER1
 # openshift_password1 / CONF_OPENSHIFT_PASSWORD1
 #   Default: demo/changeme
 #   This user and password are entered in the /etc/openshift/htpasswd
 #   file as a demo/test user. You will likely want to remove it after
 #   installation (or just use a different auth method).
+#CONF_OPENSHIFT_USER1="demo"
+#CONF_OPENSHIFT_PASSWORD1="changeme"
 
 # IMPORTANT NOTES - DEPENDENCIES
 #
@@ -430,7 +453,7 @@ install_cartridges()
   # JBossEAP6.0 support.
   # Note: Be sure to subscribe to the JBossEAP entitlements during the
   # base install or in configure_jbosseap_subscription.
-  #carts="$carts openshift-origin-cartridge-jbosseap-6.0"
+  carts="$carts openshift-origin-cartridge-jbosseap-6.0"
 
   # Jenkins server for continuous integration.
   carts="$carts openshift-origin-cartridge-jenkins-1.4"
@@ -461,7 +484,10 @@ install_cartridges()
 
   # When dependencies are missing, e.g. JBoss subscriptions,
   # still install as much as possible.
-  carts="$carts --skip-broken"
+  if [ "$CONF_OPTIONAL_REPO" == 1 ]
+  then
+    carts="$carts --skip-broken"
+  fi
 
   yum install -y $carts
 }
@@ -661,7 +687,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
+  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF
@@ -1771,7 +1797,7 @@ configure_console_msg
 is_false "$CONF_NO_NTP" && synchronize_clock
 is_false "$CONF_NO_SSH_KEYS" && install_ssh_keys
 
-if is_false "$CONF_NO_REPOS"
+if [ "$CONF_INSTALL_METHOD" == "yum" ]
 then
   configure_rhel_repo
   if activemq || broker || datastore
@@ -1780,9 +1806,27 @@ then
   fi
   node && configure_node_repo
   node && configure_jbosseap_cartridge_repo
-  node && configure_jbosseap_subscription
-  node && configure_jbossews_subscription
   broker && configure_client_tools_repo
+elif [ "$CONF_INSTALL_METHOD" == "rhn" ]
+then
+   echo "Register with RHN using an activation key"
+   rhnreg_ks --activationkey=${CONF_RHN_REG_ACTKEY} --profilename=${hostname}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-rhc --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   broker && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-infrastructure --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-node --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel rhel-x86_64-server-6-osop-1-jbosseap --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jbappplatform-6-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   node && rhn-channel --add --channel jb-ews-1-x86_64-server-6-rpm --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   if [ "$CONF_OPTIONAL_REPO" == 1 ]
+   then
+     rhn-channel --add --channel rhel-x86_64-server-optional-6 --user ${CONF_RHN_REG_NAME} --password ${CONF_RHN_REG_PASS}
+   fi
+elif [ "$CONF_INSTALL_METHOD" == "sm" ]
+then
+# sm_reg_name / CONF_SM_REG_NAME
+# sm_reg_pass / CONF_SM_REG_PASS
+# sm_reg_pool / CONF_SM_REG_POLL
+   echo "sam"
 fi
 
 yum update -y

commit d3c67d27e7d750678a4c71597b31a757a42ec56d

Fix usage of is_false
Commit 8b2d53cd722590d6142872ca4b175b24ff7f8588 introduced an incorrect
usage of is_false, combining it with the [ function.  The is_false function
itself returns a truth value and does not need to be combined with [.  This
commit fixes the misusage.

enterprise/install-scripts/amazon/openshift-amz.sh
@@ -438,7 +438,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
+  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF

enterprise/install-scripts/generic/openshift.ks
@@ -741,7 +741,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
+  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF

enterprise/install-scripts/generic/openshift.sh
@@ -693,7 +693,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
+  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF

enterprise/install-scripts/openshift.ks
@@ -717,7 +717,7 @@ configure_datastore()
   done
   echo "MongoDB is ready! ($(date +%H:%M:%S))"
 
-  if [ is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST" ]
+  if is_false "$CONF_NO_DATASTORE_AUTH_FOR_LOCALHOST"
   then
     # Add an administrative user and a user that the broker will use.
     mongo <<EOF