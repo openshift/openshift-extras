5b9e170f95d87d81401adfe972b09b97e811723c
N. Harrison Ripps <hripps@redhat.com>
2013-10-03 12:04:04 -0400

    Fixed packaging and runtime environment setup for installer bootstrap

--- oo-install/oo-install-bootstrap.sh
+++ oo-install/oo-install-bootstrap.sh
@@ -13,9 +13,19 @@ echo "Extracting oo-install to temporary directory..."
 unzip -qq -o ${TMPDIR}oo-install.zip -d $TMPDIR
 
 echo "Starting oo-install..."
+RUBYDIR='1.9.1'
+RUBYVER=`ruby -v`
+if [[ $RUBYVER == ruby\ 1\.8* ]]
+then
+  RUBYDIR='1.8'
+fi
+GEM_PATH="${TMPDIR}oo-install/vendor/bundle/ruby/${RUBYDIR}/gems/"
 RUBYLIB="${TMPDIR}oo-install/lib:${TMPDIR}oo-install/vendor/bundle"
-cd ${TMPDIR}oo-install && RUBYLIB=$RUBYLIB sh -c "bin/oo-install $@"
-cd -
+for i in `ls $GEM_PATH`
+do
+  RUBYLIB="${RUBYLIB}:${GEM_PATH}${i}/lib/"
+done
+GEM_PATH=$GEMPATH RUBYLIB=$RUBYLIB sh -c "${TMPDIR}oo-install/bin/oo-install $@"
 
 echo "oo-install exited; removing temporary assets."
 rm -rf ${TMPDIR}oo-install*
