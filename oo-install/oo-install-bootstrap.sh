9ab2cd5d731f9b067bf39a1c59f9ecdd6593bc3e
N. Harrison Ripps <hripps@redhat.com>
2013-10-03 09:39:34 -0400

    Bundler standalone and bootsrapper

--- oo-install/oo-install-bootstrap.sh
+++ oo-install/oo-install-bootstrap.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
+# Grab command-line arguments
+args=("$@")
+
 : ${TMPDIR:=/tmp}
-[[ $TMPDIR != */ ]] && TMPDIR="$TMPDIR"/
+[[ $TMPDIR != */ ]] && TMPDIR="${TMPDIR}/"
 
-file_prefix="openshift-openshift-extras-*"
+echo "Downloading oo-install package..."
 #curl -L -o ${TMPDIR}openshift-extras.zip https://api.github.com/repos/nhr/openshift-extras/zipball/master
-unzip -o ${TMPDIR}oo-install.zip -d $TMPDIR
 
-export GEM_HOME=${TMPDIR}oo-install/vendor/bundle/ruby/1.9.1
-export GEM_PATH=${TMPDIR}oo-install/vendor/bundle/ruby/1.9.1
-export RUBYLIB=${TMPDIR}$oo-install/lib:${TMPDIR}oo-install/vendor/bundle/ruby/1.9.1/gems/*/lib
+echo "Extracting oo-install to temporary directory..."
+unzip -qq -o ${TMPDIR}oo-install.zip -d $TMPDIR
 
-cd ${TMPDIR}oo-install && bin/oo-install -e
+echo "Starting oo-install..."
+RUBYLIB="${TMPDIR}oo-install/lib:${TMPDIR}oo-install/vendor/bundle"
+cd ${TMPDIR}oo-install && RUBYLIB=$RUBYLIB sh -c "bin/oo-install $@"
 cd -
-#rm -rf ${TMPDIR}openshift-*
+
+echo "oo-install exited; removing temporary assets."
+#rm -rf ${TMPDIR}oo-install*
 
 exit
