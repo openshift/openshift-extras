e8edfea8df2e7f9694ec349b79d633558b3ebedf
dobbymoodge <jolamb@redhat.com>
2013-10-22 13:21:06 -0400

    Refactor oo-admin-check-sources.py, add repo_db.py for resolving repo attributes
    This commit adds the repo_db module, which provides functions for
    resolving repoids or lists of repoids against lists of "blessed" repos
    and their expected or assigned attributes. For example, you can use
    the enabled repos + installed packages on a system to guess the
    version (by comparing against repoid and key_pkg attrs) and then use
    that to verify the version and roles specified by a user.
    
    oo-admin-check-sources.py has been rewritten to use repo_db, and also
    changes the RHSM/RHN entitlement handling. Now "hybrid" systems aren't
    supported, but rather for systems with both RHSM and RHN
    subscriptions, RHSM is preferred unless all OpenShift channels are
    provided by RHN.
    
    This revision also adds version detection, so theoretically OpenShift
    Enterprise 2.0 should be supported.

---
+++ admin/check-sources/repo_db.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python -tt
+from collections import namedtuple
+from copy import copy
+
+RepoTuple = namedtuple('RepoTuple', 'subscription, product, product_version, role, repoid, key_pkg')
+
+repositories = [
+    # RHSM Common
+    RepoTuple(subscription = 'rhsm',
+              product = 'rhel',
+              product_version = ('1.2', '2.0'),
+              role = 'base',
+              repoid = 'rhel-6-server-rpms',
+              key_pkg = None),
+    RepoTuple(subscription = 'rhsm',
+              product = 'jboss',
+              product_version = ('1.2', '2.0'),
+              role = 'node',
+              repoid = 'jb-ews-2-for-rhel-6-server-rpms',
+              key_pkg = 'openshift-origin-cartridge-jbossews'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'jboss',
+              product_version = ('1.2', '2.0'),
+              role = 'node-eap',
+              repoid = 'jb-eap-6-for-rhel-6-server-rpms',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repositories += [
+    # RHSM 1.2 repos
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'node',
+              repoid = 'rhel-server-ose-1.2-node-6-rpms',
+              key_pkg = 'rubygem-openshift-origin-node'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'broker',
+              repoid = 'rhel-server-ose-1.2-infra-6-rpms',
+              key_pkg = 'openshift-origin-broker'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'client',
+              repoid = 'rhel-server-ose-1.2-rhc-6-rpms',
+              key_pkg = 'rhc'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'node-eap',
+              repoid = 'rhel-server-ose-1.2-jbosseap-6-rpms',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repositories += [
+    # RHSM 2.0 repos
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'node',
+              repoid = 'rhel-server-ose-2.0-node-6-rpms',
+              key_pkg = 'rubygem-openshift-origin-node'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'broker',
+              repoid = 'rhel-server-ose-2.0-infra-6-rpms',
+              key_pkg = 'openshift-origin-broker'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'client',
+              repoid = 'rhel-server-ose-2.0-rhc-6-rpms',
+              key_pkg = 'rhc'),
+    RepoTuple(subscription = 'rhsm',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'node-eap',
+              repoid = 'rhel-server-ose-2.0-jbosseap-6-rpms',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repositories += [
+    # RHN Common
+    RepoTuple(subscription = 'rhn',
+              product = 'rhel',
+              product_version = ('1.2', '2.0'),
+              role = 'base',
+              repoid = 'rhel-x86_64-server-6',
+              key_pkg = None),
+    RepoTuple(subscription = 'rhn',
+              product = 'jboss',
+              product_version = ('1.2', '2.0'),
+              role = 'node',
+              repoid = 'jb-ews-2-x86_64-server-6-rpm',
+              key_pkg = 'openshift-origin-cartridge-jbossews'),
+    RepoTuple(subscription = 'rhn',
+              product = 'jboss',
+              product_version = ('1.2', '2.0'),
+              role = 'node-eap',
+              repoid = 'jbappplatform-6-x86_64-server-6-rpm',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repositories += [
+    # RHN 1.2 repos
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'node',
+              repoid = 'rhel-x86_64-server-6-ose-1.2-node',
+              key_pkg = 'rubygem-openshift-origin-node'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'broker',
+              repoid = 'rhel-x86_64-server-6-ose-1.2-infrastructure',
+              key_pkg = 'openshift-origin-broker'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'client',
+              repoid = 'rhel-x86_64-server-6-ose-1.2-rhc',
+              key_pkg = 'rhc'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '1.2',
+              role = 'node-eap',
+              repoid = 'rhel-x86_64-server-6-ose-1.2-jbosseap',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repositories += [
+    # RHN 2.0 repos
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'node',
+              repoid = 'rhel-x86_64-server-6-ose-2.0-node',
+              key_pkg = 'rubygem-openshift-origin-node'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'broker',
+              repoid = 'rhel-x86_64-server-6-ose-2.0-infrastructure',
+              key_pkg = 'openshift-origin-broker'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'client',
+              repoid = 'rhel-x86_64-server-6-ose-2.0-rhc',
+              key_pkg = 'rhc'),
+    RepoTuple(subscription = 'rhn',
+              product = 'ose',
+              product_version = '2.0',
+              role = 'node-eap',
+              repoid = 'rhel-x86_64-server-6-ose-2.0-jbosseap',
+              key_pkg = 'openshift-origin-cartridge-jbosseap'),
+]
+
+repo_cache = {}
+
+def _repo_tuple_match(repo, match_attr, match_val):
+    # Could do this on one line, but this is more readable
+    attr = getattr(repo, match_attr, None)
+    if match_val == attr:
+        return True
+    return hasattr(attr, '__iter__') and match_val in attr
+
+def find_repos(**kwargs):
+    global repo_cache
+    if len(repo_cache) > 512:
+        # Try to keep the repo_cache from growing infinitely
+        # (Guessing 512 is too big is cheaper than calculating the actual mem footprint)
+        repo_cache = {}
+    hkey = tuple(sorted(kwargs.items()))
+    if None == repo_cache.get(hkey, None):
+        repos = copy(repositories)
+        for kk, vv in kwargs.items():
+            # print kk, vv
+            repos = [repo for repo in repos if _repo_tuple_match(repo, kk, vv)]
+            # print repos
+            if not repos:
+                break
+        repos = tuple(repos)
+        repo_cache[hkey] = repos
+        return repos
+    return repo_cache[hkey]
+
+def find_repoids(**kwargs):
+    return [repo.repoid for repo in find_repos(**kwargs)]
+
+def find_repos_by_repoid(repoids):
+    if hasattr(repoids, '__iter__'):
+        res = []
+        for r_id in repoids:
+            res += find_repos(repoid = r_id)
+        return tuple(set(res)) # make unique
+    return find_repos(repoid = repoids)
+
+if __name__ == '__main__':
+    print "find_repos(subscription = 'rhsm'):"
+    for xx in find_repos(subscription = 'rhsm'):
+        print xx
+    print ""
+    print "find_repos(product_version = '1.2'):"
+    for xx in find_repos(product_version = '1.2'):
+        print xx
+    print ""
+    print "find_repos(product_version = '1.2', role = 'node'):"
+    for xx in find_repos(product_version = '1.2', role = 'node'):
+        print xx
+    print ""
+    print "find_repos_by_repoid('rhel-x86_64-server-6-ose-2.0-infrastructure'):"
+    for xx in find_repos_by_repoid('rhel-x86_64-server-6-ose-2.0-infrastructure'):
+        print xx
+    print ""
+    print "find_repos_by_repoid(['rhel-x86_64-server-6-ose-2.0-infrastructure', 'jbappplatform-6-x86_64-server-6-rpm']):"
+    for xx in find_repos_by_repoid(['rhel-x86_64-server-6-ose-2.0-infrastructure', 'jbappplatform-6-x86_64-server-6-rpm']):
+        print xx
+    print ""
+    print "find_repoids(subscription = 'rhn', role = 'node-eap', product_version = '1.2'):"
+    for xx in find_repoids(subscription = 'rhn', role = 'node-eap', product_version = '1.2'):
+        print xx
