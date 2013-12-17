0d3b6c34090ec6f6917fa32b3dc94c295eca48fb
Luke Meyer <lmeyer@redhat.com>
2013-12-17 13:07:11 -0500

    <sample routing listener> bug 1043394 + other minor fixes

--- admin/sample_routing_listener.rb
+++ admin/sample_routing_listener.rb
@@ -2,7 +2,7 @@
 
 
 ## Guide to writing a router that listens to the Openshift activemq routing plugin
-#  Step 1  : Listen to activemq topic 'routinginfo'. 
+#  Step 1  : Listen to activemq topic 'routinginfo'.
 #            Note that the activemq routing plugin has to be configured with the same creds (routinginfo/routinginfopasswd)
 #            Also note that host/port are hardcoded in this script
 #  Step 2  : The following events from Openshift as the applications go through their life cycle
@@ -14,12 +14,12 @@
 #            Clearly, if the router plans to route to load_balancer ones, then the web_framework ones should be inactive.
 #            Unless the situation is that all load_balancer endpoints are down.
 #  Step 3c : Use the 'mappings' field to identify url routes. The non-root routes may require special treatment like source-ip verification,
-#            no HA load balancing, ssl_cert etc. 
+#            no HA load balancing, ssl_cert etc.
 #            Common use case will be admin consoles like phpmyadmin. (Details for this are yet to be sorted out.)
 #  Step 4  : Look at the ssl_cert notifications for the application, and whether to configure the router accordingly for requests coming in
 #  Step 5  : Finally look at alias notifications. All aliases will need to be accomodated at any time during the application's lifecycle.
-#  
-#  Note : add_gear/delete_gear do not correspond to actual addition and removal of gear, but that of endpoints. 
+#
+#  Note : add_gear/delete_gear do not correspond to actual addition and removal of gear, but that of endpoints.
 #  One gear added to OpenShift application may result in several endpoints being created, and they will all result in respective add_gear notifications at the router level.
 
 
@@ -29,7 +29,7 @@
 #           Look for delete_gear/load_balancer message and edit the nginx config file
 #           Look for delete_application message and remove the nginx config file
 ##
-           
+
 require 'rubygems'
 require 'stomp'
 require 'yaml'
@@ -40,7 +40,7 @@ def add_haproxy(appname, namespace, ip, port)
   scope = "#{appname}-#{namespace}"
   file = File.join(CONF_DIR, "#{scope}.conf")
   if File.exist?(file)
-    `sed -i 's/upstream #{scope} {/&\n      server #{ip}:#{port}' #{file}`
+    `sed -i 's/upstream #{scope} {/&\\n      server #{ip}:#{port}' #{file}`
   else
     # write a new one
     template = <<-EOF
@@ -61,10 +61,10 @@ EOF
 end
 
 
-c = Stomp::Client.new("routinginfo", "routinginfopasswd", "localhost", 6163, true)
+c = Stomp::Client.new("routinginfo", "routinginfopasswd", "localhost", 61613, true)
 c.subscribe('/topic/routinginfo') { |msg|
   h = YAML.load(msg.body)
-  if h[:action] == :add_gear 
+  if h[:action] == :add_gear
     if h[:types].include? "load_balancer"
        add_haproxy(h[:app_name], h[:namespace], h[:public_address], h[:public_port])
        puts "Added routing endpoint for #{h[:app_name]}-#{h[:namespace]}"
