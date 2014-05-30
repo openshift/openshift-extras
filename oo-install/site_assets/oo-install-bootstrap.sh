619aa17afba1d2afd9601f1f70c68673ca17714d
N. Harrison Ripps <hripps@redhat.com>
2014-05-30 12:12:43 -0400

    Use tar for packaging instead of zip
    Conflicts:
    	oo-install/site_assets/oo-install-bootstrap.sh

--- oo-install/site_assets/oo-install-bootstrap.sh
+++ oo-install/site_assets/oo-install-bootstrap.sh
@@ -8,27 +8,27 @@ cmdlnargs="$@"
 [[ $TMPDIR != */ ]] && TMPDIR="${TMPDIR}/"
 
 echo "Checking for necessary tools..."
-for i in ruby unzip ssh
+for i in ruby ssh
 do
   command -v $i >/dev/null 2>&1 || { echo >&2 "OpenShift installation requires $i but it does not appear to be available. Correct this and rerun the installer."; exit 1; }
 done
 echo "...looks good."
 
 # All instances of INSTALLPKGNAME are replaced during packaging with the actual package name.
-if [[ -e ./INSTALLPKGNAME.zip ]]
+if [[ -e ./INSTALLPKGNAME.tgz ]]
 then
   echo "Using bundled assets."
-  cp INSTALLPKGNAME.zip ${TMPDIR}/INSTALLPKGNAME.zip
-elif [[ $OO_INSTALL_KEEP_ASSETS == 'true' && -e ${TMPDIR}/INSTALLPKGNAME.zip ]]
+  cp INSTALLPKGNAME.tgz ${TMPDIR}/INSTALLPKGNAME.tgz
+elif [[ $OO_INSTALL_KEEP_ASSETS == 'true' && -e ${TMPDIR}/INSTALLPKGNAME.tgz ]]
 then
   echo "Using existing installer assets."
 else
   echo "Downloading oo-install package..."
-  curl -s -o ${TMPDIR}INSTALLPKGNAME.zip https://install.openshift.com/INSTALLVERPATHINSTALLPKGNAME.zip
+  curl -s -o ${TMPDIR}INSTALLPKGNAME.tgz https://install.openshift.com/INSTALLVERPATHINSTALLPKGNAME.tgz
 fi
 
 echo "Extracting oo-install to temporary directory..."
-unzip -qq -o ${TMPDIR}INSTALLPKGNAME.zip -d $TMPDIR
+tar xzf ${TMPDIR}INSTALLPKGNAME.tgz -C $TMPDIR
 
 echo "Starting oo-install..."
 RUBYDIR='1.9.1'
