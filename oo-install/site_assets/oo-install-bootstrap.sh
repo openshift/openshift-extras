9d8991a3e715763dc0bb16fa08a7b9fecb9d4822
Brenton Leanhardt <bleanhar@redhat.com>
2015-06-05 16:08:25 -0400

    virtualenv is required

--- oo-install/site_assets/oo-install-bootstrap.sh
+++ oo-install/site_assets/oo-install-bootstrap.sh
@@ -13,7 +13,7 @@ if [ $OO_INSTALL_CONTEXT != 'origin_vm' ]
 then
   echo "Checking for necessary tools..."
 fi
-for i in python ssh
+for i in python virtualenv ssh
 do
   command -v $i >/dev/null 2>&1 || { echo >&2 "OpenShift installation requires $i but it does not appear to be available. Correct this and rerun the installer."; exit 1; }
 done
