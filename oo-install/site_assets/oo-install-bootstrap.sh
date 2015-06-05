c3a0296d7d7e887e5b76463c9c58fd57f8592bad
Brenton Leanhardt <bleanhar@redhat.com>
2015-06-05 10:42:20 -0400

    the bootstrapper doesn't use RPM names

--- oo-install/site_assets/oo-install-bootstrap.sh
+++ oo-install/site_assets/oo-install-bootstrap.sh
@@ -12,7 +12,7 @@ if [ $OO_INSTALL_CONTEXT != 'origin_vm' ]
 then
   echo "Checking for necessary tools..."
 fi
-for i in python PyYAML ssh
+for i in python ssh
 do
   command -v $i >/dev/null 2>&1 || { echo >&2 "OpenShift installation requires $i but it does not appear to be available. Correct this and rerun the installer."; exit 1; }
 done
