568202a23f48a3b3dbe1f65ac68c7426e90580b2
Brenton Leanhardt <bleanhar@redhat.com>
2015-06-22 15:29:29 -0400

    Removing the hardcoded git tag

--- oo-install/site_assets/oo-install-bootstrap.sh
+++ oo-install/site_assets/oo-install-bootstrap.sh
@@ -70,7 +70,7 @@ then
 else
   clear
 fi
-oo-install --ansible-playbook-directory ${TMPDIR}/INSTALLPKGNAME/openshift-ansible-3.0.0-rc/ --ansible-log-path $OO_INSTALL_LOG
+oo-install --ansible-playbook-directory ${TMPDIR}/INSTALLPKGNAME/openshift-ansible-*/ --ansible-log-path $OO_INSTALL_LOG
 
 if [ $OO_INSTALL_KEEP_ASSETS == 'true' ]
 then
