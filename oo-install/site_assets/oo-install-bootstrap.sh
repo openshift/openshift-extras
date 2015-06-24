b38d3e70a8725311b0627a01a7f40bc76933b2ef
Brenton Leanhardt <bleanhar@redhat.com>
2015-06-24 16:58:21 -0400

    Fixing dev links that bled in to GA

--- oo-install/site_assets/oo-install-bootstrap.sh
+++ oo-install/site_assets/oo-install-bootstrap.sh
@@ -5,10 +5,10 @@ cmdlnargs="$@"
 
 clear
 echo "For Origin M4 please run:"
-echo "sh <(curl -s https://ootest-leanhardt.rhcloud.com/origin-m4)"
+echo "sh <(curl -s https://install.openshift.com/origin-m4)"
 echo
 echo "For OpenShift Enterprise 3 run:"
-echo "sh <(curl -s https://ootest-leanhardt.rhcloud.com/ose)"
+echo "sh <(curl -s https://install.openshift.com/ose)"
 echo
 echo "An Origin oo-install script for the latest code is still being developed."
 echo "See https://github.com/openshift/origin/blob/master/README.md#getting-started"
